a3dbb52fd9a07739882e865313bf89a9fd8119db
masaltsev <a.o.masaltsev@gmail.com>
2023-02-08 23:03:09 +0200

    user service methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
-import service.UserService;
 import service.impl.UserServiceImpl;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> countries = List.of("Moldova", "Romania");
-        UserService userService = new UserServiceImpl();
-        userService.displayFilteredUsers(countries, 223, 1);
+        List<String> tags = List.of("java", ".net", "docker", "C#");
+        new UserServiceImpl().displayFilteredUsers(countries, 100000, 1, tags);
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,5 +3,6 @@ package service;
 import java.util.List;
 
 public interface UserService {
-    void displayFilteredUsers(List<String> countries, int reputationMin, int answerCountMin);
+    void displayFilteredUsers(List<String> countries, int reputationMin,
+                              int answerCountMin, List<String> tags);
 }
