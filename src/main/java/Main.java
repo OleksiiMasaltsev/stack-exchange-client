b22339905f5bdd750065d54558705de170c1b6cc
masaltsev <a.o.masaltsev@gmail.com>
2023-02-09 11:33:25 +0200

    Cleaning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         List<String> countries = List.of("Moldova", "Romania");
         List<String> tags = List.of("java", ".net", "docker", "C#");
-        new UserServiceImpl().displayFilteredUsers(countries, 100000, 1, tags);
+        new UserServiceImpl().displayFilteredUsers(countries, 233, 1, tags);
     }
 }
---
+++ src/test/java/service/impl/UserServiceImplTest.java
@@ -0,0 +1,23 @@
+package service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.RootHttpClient;
+
+class UserServiceImplTest {
+    private UserServiceImpl userService;
+    private RootHttpClient httpClient;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void displayFilteredUsers() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
