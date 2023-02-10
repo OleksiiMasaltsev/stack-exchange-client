2df1aab374124edb4eb4e57fdd369590d064b629
masaltsev <a.o.masaltsev@gmail.com>
2023-02-10 16:00:12 +0200

    output varification, string format in the user class

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -5,9 +5,12 @@ import model.Collective;
 import model.CollectiveItem;
 import model.ResponseWrapper;
 import model.User;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Set;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -23,7 +26,7 @@ class UserServiceTest {
     }
 
     @Test
-    void displayFilteredUsers_twoValidUsers_printOnlyOne() {
+    void displayFilteredUsers_twoValidUsers_printsOnlyOne() {
         Set<String> tags = Set.of("azure-digital-twins",
                 "azure-secrets",
                 "azure-functions-docker",
@@ -38,7 +41,12 @@ class UserServiceTest {
         ResponseWrapper wrapper = new ResponseWrapper(Set.of(user1, user2), false);
 
         when(userHttpClient.get(anyString())).thenReturn(wrapper);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
         userService.displayFilteredUsers(Set.of("Moldova"), Set.of("java", "docker"), 223, 1);
+
+        assertEquals(user1.toString(), baos.toString().strip());
         Mockito.verify(userHttpClient).get(anyString());
     }
 
@@ -58,7 +66,12 @@ class UserServiceTest {
         ResponseWrapper wrapper = new ResponseWrapper(Set.of(user1, user2), false);
 
         when(userHttpClient.get(anyString())).thenReturn(wrapper);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
         userService.displayFilteredUsers(Set.of("Moldova"), Set.of("java", "docker"), 223, 1);
+
+        assertEquals("", baos.toString().strip());
         Mockito.verify(userHttpClient).get(anyString());
     }
 }
